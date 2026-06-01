12062f0730afe81e3d18a5eba54d47ff34314a18
Oron Ben Zvi <oronbz@gmail.com>
2026-06-01 16:29:30 +0300

    Update cousebara to 2.0.2

--- Casks/cousebara.rb
+++ Casks/cousebara.rb
@@ -1,6 +1,6 @@
 cask "cousebara" do
-  version "2.0.1"
-  sha256 "4c5f76e1bcd142337622595902189890ef2d8774921ee69d0a6c445a280b0df0"
+  version "2.0.2"
+  sha256 "983fff0bdd3f479cea33e4d4e411139dd4f5b6189e5dc2acad254365c7a6d83c"
 
   url "https://github.com/oronbz/Cousebara/releases/download/v#{version}/Cousebara.zip"
   name "Cousebara"
